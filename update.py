5837396a9885e96e0358022d867b4b124ad10249
Doritanh <charrier.antho99@gmail.com>
2021-03-15 11:34:08 +0100

    Add arguments line

--- update.py
+++ update.py
@@ -1,14 +1,22 @@
 import requests
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Simple downloader script for PaperMC.')
+parser.add_argument(
+    "-p", "--project", help="Select the project to download latest (paper, waterfall).", default="paper")
+parser.add_argument(
+    "-v", "--version", help="Select the project version (1.16, 1.15...).", default="1.16")
+args = parser.parse_args()
 
 baseURL = "https://papermc.io/api/v2/projects/"
 
 version = "1.16"
-name = "paper"
+project = args.project
 
 # r = requests.get("https://papermc.io/api/v2/projects")
 
-r = requests.get(baseURL + name + "/version_group/" + version + "/builds")
+r = requests.get(baseURL + project + "/version_group/" + version + "/builds")
 rJSON = r.json()
 
 lastBuild = rJSON['builds'][-1]
@@ -17,7 +25,8 @@ buildVersion = str(lastBuild['version'])
 buildNumber = str(lastBuild['build'])
 buildDownload = str(lastBuild['downloads']['application']['name'])
 
-downloadURL = baseURL + name + "/versions/" + buildVersion + "/builds/" + buildNumber + "/downloads/" + buildDownload
+downloadURL = baseURL + project + "/versions/" + buildVersion + \
+    "/builds/" + buildNumber + "/downloads/" + buildDownload
 
 r = requests.get(downloadURL)
 
