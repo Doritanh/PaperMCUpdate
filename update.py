e1715225b190d4bc6785b1bb02b89f4eb4a67cd0
Doritanh <charrier.antho99@gmail.com>
2021-03-15 11:15:06 +0100

    Better source of thruth

--- update.py
+++ update.py
@@ -1,12 +1,14 @@
 import requests
 import json
 
+baseURL = "https://papermc.io/api/v2/projects/"
+
 version = "1.16"
 name = "paper"
 
 # r = requests.get("https://papermc.io/api/v2/projects")
 
-r = requests.get("https://papermc.io/api/v2/projects/" + name + "/version_group/" + version + "/builds")
+r = requests.get(baseURL + name + "/version_group/" + version + "/builds")
 rJSON = r.json()
 
 lastBuild = rJSON['builds'][-1]
@@ -15,7 +17,7 @@ buildVersion = str(lastBuild['version'])
 buildNumber = str(lastBuild['build'])
 buildDownload = str(lastBuild['downloads']['application']['name'])
 
-downloadURL = "https://papermc.io/api/v2/projects/paper/versions/" + buildVersion + "/builds/" + buildNumber + "/downloads/" + buildDownload
+downloadURL = baseURL + name + "/versions/" + buildVersion + "/builds/" + buildNumber + "/downloads/" + buildDownload
 
 r = requests.get(downloadURL)
 
