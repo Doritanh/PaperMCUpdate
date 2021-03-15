74155bd1b55a7198f51d898171e121b52480a202
Doritanh <charrier.antho99@gmail.com>
2021-03-15 10:53:05 +0100

    Generate download URL

--- paper.py
+++ paper.py
@@ -4,7 +4,13 @@ import json
 version = "1.16"
 
 r = requests.get("https://papermc.io/api/v2/projects/paper/version_group/" + version + "/builds")
+# r = requests.get("https://papermc.io/api/v2/projects")
 rJSON = r.json()
 
-json_formatted_str = json.dumps(rJSON, indent=4)
-print(json_formatted_str)
+lastBuild = rJSON['builds'][-1]
+
+buildVersion = str(lastBuild['version'])
+buildNumber = str(lastBuild['build'])
+buildDownload = str(lastBuild['downloads']['application']['name'])
+
+print("https://papermc.io/api/v2/projects/paper/versions/" + buildVersion + "/builds/" + buildNumber + "/downloads/" + buildDownload)
